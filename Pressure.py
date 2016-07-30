P small bug in calculation of spatial pressure

Pressure.py
@@ -27,6 +27,7 @@ def main():
 			--nosave	False
 		-a	--plotall	False
 	"""
+	
 	me = "Pressure.main: "
 	t0 = time()
 	
@@ -211,16 +212,19 @@ def pressure_dir(dirpath, nosave, verbose):
 		A[i], B[i] = a, b
 		
 	## ------------------------------------------------	
-	
-	## Phase diagram plot
+	## Normalise pressure
 	
 	P /= P_eq
 	
-	figtit = "Pressure (normalised)"
-	fsa, fsl, fst = 16,12,18
+	## ------------------------------------------------	
+	## Phase diagram plot
+	
+	plotfile = dirpath+"/PRESSab_scatter.jpg"
+	figtit = "Pressure ($P_{\\rm eq}(\\alpha,\\beta)=1.0$)"
+	fsa, fsl, fst = 16,12,16
 
 	fig = plt.gcf(); ax = plt.gca()
-	plt.scatter(A, B, marker="o", c=P, s=500*P/P.max(), edgecolor="None", label="RT")
+	plt.scatter(A, B, marker="o", c=np.log(P), s=500*P/P.max(), edgecolor="None", label="RT")
 	plt.colorbar()
 	
 	ax.set_xscale("log");	ax.set_yscale("log")
@@ -230,14 +234,17 @@ def pressure_dir(dirpath, nosave, verbose):
 	ax.set_ylabel("$\\beta$",fontsize=fsa)
 	ax.grid()
 
-	fig.suptitle(figtit)
+	fig.suptitle(figtit, fontsize=fst)
 
 	## ------------------------------------------------	
-	## SAVING
+	## Lines plot
 	
+	
+	
+	## ------------------------------------------------	
+	## Save
 	
 	if not nosave:
-		plotfile = dirpath+"/PRESSab_scatter.jpg"
 		fig.savefig(plotfile, dpi=2*fig.dpi)
 		if verbose: print me+"Plot saved to",plotfile
 
@@ -292,7 +299,7 @@ def calc_pressure(r,rho,r_eq,U,spatial=False):
 	
 	## Pressure
 	if spatial == True:
-		P = -np.array([np.trapz(force[:i]*rho[:i], r[:i]) for i in xrange(r.size)])
+		P = -np.array([np.trapz(force[:i]*rho[:i], r[:i]) for i in range(1,r.size+1)])
 	else:
 		P = -np.trapz(force*rho, r)
 	

persistant_motion_2D_PE_landscape.py
@@ -11,15 +11,16 @@ def main(alpha, beta):
 	##initial stuff (very specific, I know, but those four variables... that are like, muy importante and related to alpha and beta)
 	##alpha = (tau_av)*(prop_force)/radius
 	##beta = k*radius/(prop_force)
+	
 	k = 1.0
 	prop_force_i = 1.0
 	r = beta*prop_force_i/k
 	tau_av = alpha*r/prop_force_i
 	##time variables 
 	dt = 0.01
-	tmax = 50000.0
+	tmax = 5e5
 	##time switch variables 
-	number_of_switches = 2.0*tmax/tau_av
+	number_of_switches = 1.5*tmax/tau_av
 	
 	## array of times
 	tx = np.arange(0.00,tmax,dt)
@@ -86,8 +87,9 @@ def main(alpha, beta):
 	histfilename = "Datafiles/HISDAT_2D_alpha%s_beta%s_tmax%s" %(alpha, beta, tmax)
 	np.savez(histfilename, H_rad=H_rad,bin_center_rad=bin_centers,domain_rad=domain_rad, U_landscape=U_landscape, prob_eq=prob_eq,alpha=alpha, beta=beta, tmax=tmax)
 	
-	plot2d(histfilename+".npz")
-	plt.close()
+	# plt.close()
+	# plot2d(histfilename+".npz")
+	# plt.close()
 	
 	return
 